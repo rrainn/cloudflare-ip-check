f364ec9073b92bd84730518854b1fc99fd667845
Charlie Fish <fishcharlie@me.com>
2022-08-13 15:56:40 -0600

    Basic code

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # cloudflare-ip-check
+
+## Summary
+
+This npm package is used to check if a certain IP came from Cloudflare. The list of Cloudflare IPs is updated every day.
+
+## Install
+
+```
+npm install https://github.com/rrainn/cloudflare-ip-check.git
+```
+
+## Usage
+
+```js
+const cloudflareIPCheck = require("cloudflare-ip-check");
+```
+
+### cloudflareIPs
+
+Array of strings representing every Cloudflare IP range.
+
+```js
+console.log(cloudflareIPCheck.cloudflareIPs);
+```
+
+### v4cloudflareIPs
+
+Array of strings representing every v4 Cloudflare IP range.
+
+```js
+console.log(cloudflareIPCheck.v4cloudflareIPs);
+```
+
+### v6cloudflareIPs
+
+Array of strings representing every v6 Cloudflare IP range.
+
+```js
+console.log(cloudflareIPCheck.v6cloudflareIPs);
+```
+
+### isFromCloudflare(ip)
+
+Check if an IP is from Cloudflare, returns a boolean representing if an IP is from Cloudflare.
+
+```js
+console.log(cloudflareIPCheck.isFromCloudflare("0.0.0.0")); // false
+console.log(cloudflareIPCheck.isFromCloudflare("173.245.48.2")); // true
+```
+
+### middleware([configuration])
+
+This is an Express.js middleware function that can be used to check if an IP is from Cloudflare.
+
+```js
+app.use(cloudflareIPCheck.middleware());
+```
+
+You can optionally pass in an object representing the configuration of the middleware.
+
+```js
+app.use(cloudflareIPCheck.middleware({
+	"error": {
+		"status": 403,
+		"send": "Forbidden"
+	}
+}));
+```
+
+If no configuration is passed in, it will return 403 "Forbidden" if the IP is not from Cloudflare.
---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const path = require("path");
+const range_check = require("range_check");
+
+const ips = module.exports.cloudflareIPs = ["v4.txt", "v6.txt"].map((file) => path.join(__dirname, "data", file)).map(fs.readFileSync).join("\n").split("\n");
+const v4ips = module.exports.v4CloudflareIPs = ["v4.txt"].map((file) => path.join(__dirname, "data", file)).map(fs.readFileSync).join("\n").split("\n");
+const v6ips = module.exports.v6CloudflareIPs = ["v6.txt"].map((file) => path.join(__dirname, "data", file)).map(fs.readFileSync).join("\n").split("\n");
+
+const isFromCloudflare = module.exports.isFromCloudflare = function(ip) {
+	if (range_check.isIP(ip)) {
+		const ipVersion = range_check.version(ip);
+		if (ipVersion === 4) {
+			return range_check.inRange(ip_address, v4ips);
+		} else if (ip_ver === 6) {
+			return range_check.inRange(ip_address, v6ips);
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
+};
+
+module.exports.middleware = function(configuration) {
+	return function(req, res, next) {
+		if (typeof req.headers["cf-connecting-ip"] !== "undefined" && isFromCloudflare(req.ip)) {
+			return next();
+		} else {
+			if (configuration) {
+				if (!configuration.error) {
+					console.error("`error` property missing from cloudflare-ip-check middleware configuration object. Using default response...");
+				} else {
+					if (!configuration.error.status) {
+						console.error("`error.status` property missing from cloudflare-ip-check middleware configuration object. Using default response...");
+					}
+					if (!configuration.error.send) {
+						console.error("`error.send` property missing from cloudflare-ip-check middleware configuration object. Using default response...");
+					}
+
+					if (configuration.error.status && configuration.error.send) {
+						return res.status(configuration.status).send(configuration.send);
+					}
+				}
+			}
+
+			return res.status(403).send("Forbidden");
+		}
+	}
+};
